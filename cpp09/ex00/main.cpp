2013d1e943b19b9cad09e74035d32cc0dd375a5c
WilkPrintes <wilkp90@gmail.com>
2023-10-28 12:03:31 -0300

    feat: get input file by argv, and add funcional search by less date if date dont exist in database

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -4,6 +4,7 @@
 #include <cstring>
 #include <map>
 #include <cstdlib>
+#include <ctime>
 
 void ft_split(std::string str, const char delimiter, std::map<std::string, double> &result)
 {
@@ -25,35 +26,116 @@ void ft_split(std::string str, const char delimiter, std::map<std::string, doubl
 
 void keys(std::map<std::string, double> result)
 {
-    for (std::map<std::string, double>::iterator it = result.end(); it != result.begin(); it--)
+    for (std::map<std::string, double>::iterator it = result.begin(); it != result.end(); it++)
     {
-        std::cout << "key: |" + it->first << std::endl; 
+        std::cout << "key: |" + it->first << std::endl;
     }
 }
 
-void findDate(std::string date){
+std::string ft_itoa(int value)
+{
+    std::string result;
+    int temp = value;
+    int decimal = 0;
+    int dec = 1;
+
+    while (temp > 0)
+    {
+        temp = temp / 10;
+        dec = dec * 10;
+        decimal++;
+    }
+    dec = dec / 10;
+    while (dec > 0)
+    {
+        result.push_back((value / dec) + '0');
+        value = value % dec;
+
+        dec = dec / 10;
+    }
+    return (result);
+}
+
+bool validDate(std::map<std::string, std::string> data)
+{
+    int year = atoi(data["year"].c_str());
+    int month = atoi(data["month"].c_str());
+    int day = atoi(data["day"].c_str());
+
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
+
+    if (day > 31 || day < 1)
+    {
+        std::cout << "invalid day" << std::endl;
+        return (false);
+    }
+    else if (month > 12 || month < 1)
+    {
+        std::cout << "invalid month" << std::endl;
+        return (false);
+    }
+    else if (year < 2008 && month < 10 && day < 31)
+    {
+        std::cout << "bitcoin doesn't exist yet (you're a time traveler? :O)" << std::endl;
+        return (false);
+    }
+    else if (year >= 1900 + ltm->tm_year && month >= ltm->tm_mon && day > ltm->tm_mday)
+    {
+        std::cout << "invalid date: date in future (do you have a tardis?)" << std::endl;
+        return (false);
+    }
+    else if ((year % 400 != 0) || (year % 4 != 0 && year % 100 == 0))
+    {
+        if (month == 2 && day == 29)
+        {
+            std::cout << "invalid date: not a Leap Year" << std::endl;
+            return (false);
+        }
+    }
+    else if ((month == 4 || month == 6 || month == 9 || month == 11) && day == 31)
+    {
+        std::cout << "invalid date: month with 30 days" << std::endl;
+        return (false);
+    }
+    return (true);
+}
+
+void findDate(std::string date, std::map<std::string, double> data, double value)
+{
     std::map<std::string, std::string> result;
-    const std::string keys[] = {"year", "mouth", "day"};
+    std::map<std::string, double>::iterator it;
+    const std::string keys[] = {"year", "month", "day"};
     std::string temp = "";
     int index = 0;
 
     if (date.find('-') > date.size())
-        return ;
-    for (size_t i = 0; date[i]; i++){
-        if (date[i] == '-'){
+        return;
+    for (size_t i = 0; date[i]; i++)
+    {
+        if (date[i] == '-')
+        {
             result[keys[index++]] = temp;
             i++;
             temp = "";
         }
         temp = temp + date[i];
     }
-    result[keys[index++]] = temp;
-    std::cout << result["year"] << "/" << result["mouth"] << "/" << result["day"]<< std::endl;
-}
+    result[keys[index]] = temp;
+    std::string less;
+    if (validDate(result))
+    {
+        for (it = data.begin(); it != data.end(); it++)
+        {
+            if (it->first > date || it->first == date)
+            {
+                break;
+            }
+            less = it->first;
+        }
+        std::cout << date << " -> " << value << " = " << (value * data[less]) << std::endl;
+    }
 
-void splitDate(std::string date, std::map<std::string, double> &dates){
-    std::cout << date << std::endl;
-    dates.at(0);
 }
 
 void showBtc(std::map<std::string, double> data, std::string str)
@@ -63,19 +145,27 @@ void showBtc(std::map<std::string, double> data, std::string str)
     std::string value;
     double temp;
 
-    if (str[i])
-    {
-        key = str.substr(0, i - 1);
-        value = str.substr(i + 1, str.length() - i);
-        if (data[key]){
-            temp = std::strtod(value.c_str(), NULL);
-            std::cout << "|" + key + "|" << ": " << (temp * data[key]) << std::endl;
-        }else
-            findDate(key);
+    if (str == "date | value")
+        return;
+    if (!str[i]){
+        std::cout << "invalid date" << std::endl;
+        return ;
     }
+    key = str.substr(0, i - 1);
+    value = str.substr(i + 1, str.length() - i);
+    temp = std::strtod(value.c_str(), NULL);
+    if (data[key])
+    {
+        if (temp < 0 || temp > 1000)
+            std::cout << "bitcoin out of range" << std::endl;
+        else
+            std::cout << key << " => " << temp << " = " << (temp * data[key]) << std::endl;
+    }
+    else
+        findDate(key, data, temp);
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
 
     std::ifstream dataFile;
@@ -84,18 +174,31 @@ int main(void)
     std::map<std::string, double> data;
     // std::map<std::string, double> input;
 
+    if (argc != 2){
+        std::cout << "invalid numbers of parameters" << std::endl;
+        return (-1);
+    }
+
     dataFile.open("data.csv");
     while (dataFile.good())
     {
         getline(dataFile, line);
         ft_split(line, ',', data);
     }
-    inputFile.open("input.txt");
-    
+    inputFile.open(argv[1]);
+    if (inputFile.fail()){
+        std::cout << "problem in open input file" << std::endl;
+        return (-1);
+    }
+    if (inputFile.tellg() == 0){
+        std::cout << "input file in empty" << std::endl;
+        return (-1);
+    }
     while (inputFile.good())
     {
         getline(inputFile, line);
         showBtc(data, line);
     }
     dataFile.close();
+    // std::cout << ft_itoa(1234) << std::endl;
 }
